3d1bb6c04019618f2bc3cd6d3299120341813ac4
Nalla Perumal <87609938+NallaPerumalVIKKI@users.noreply.github.com>
2022-01-29 15:35:54 +0530

    Update check.js

--- js/check.js
+++ js/check.js
@@ -5,6 +5,7 @@ email = document.getElementById('#email').value;
 function subscribe() {
     alert("Hello " + email + "Thank for reaching out");
     console.log(email);
+    
 }
 
 let validateInput = () => {
